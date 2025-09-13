1de8983551a4ab5ce169ce0f2485f4705c829787
aoi <aoi@protonmail.ch>
2025-09-13 14:49:09 -0400

    mods

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Nix-flake-based Python development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   outputs = { self, nixpkgs }:
     let
@@ -14,11 +14,14 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           venvDir = ".venv";
-          packages = with pkgs; [ python311 ] ++
+          packages = with pkgs; [ python311 neovim git ] ++
             (with pkgs.python311Packages; [
               pip
               venvShellHook
             ]);
+          shellHook = ''
+             export PS1="[nix-shell] $PS1"
+          '';
         };
       });
     };
